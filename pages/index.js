1aa9bbc9b3d6d460fdc904af2966109408b06df7
Marine Le Pennec <lepennec.marine@gmail.com>
2021-11-24 16:29:04 +0100

    Delete unused component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import MapReact from '../components/map-reactgl'
+
 function HomePage() {
   return (
-    // <MapLibre />
     <MapReact />
   )
 }
 
-HomePage.propTypes = {}
 export default HomePage
